68de1b1c5742a8dba2d6674227f2e4f59dfb26e2
Eli Rosenthal <eli_rosenthal@brown.edu>
2014-11-20 18:03:49 -0500

    adding stuff

--- vim_stuff_practice_or_whatever/test.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE LambdaCase #-}
-module Test where
-lMap :: (a -> b) -> [a] -> [b]
-lMap f = \case
-            [] -> []
-            (x:xs) ->(f x):(lMap f xs)
---
+++ workshop5-VIM/test.java
@@ -0,0 +1,65 @@
+class Main {
+  // TASK 1: remove extra lines.
+  // TASK 2: delete the line with y; it's not doing anything
+  // TASK 3: move declaration of x above the println
+  public static void main(String[] args) {
+
+
+
+
+
+
+    System.out.println(x);
+    y =
+    int x = 1;
+  }
+}
+class Main {
+  // TASK 1: remove extra lines.
+  // TASK 2: delete the line with y; it's not doing anything
+  // TASK 3: move declaration of x above the println
+  public static void main(String[] args) {
+
+
+
+
+
+
+    System.out.println(x);
+    y =
+    int x = 1;
+  }
+}
+class Main {
+  // TASK 1: remove extra lines.
+  // TASK 2: delete the line with y; it's not doing anything
+  // TASK 3: move declaration of x above the println
+  public static void main(String[] args) {
+
+
+
+
+
+
+    System.out.println(x);
+    y =
+    int x = 1;
+  }
+}
+
+class Main {
+  // TASK 1: remove extra lines.
+  // TASK 2: delete the line with y; it's not doing anything
+  // TASK 3: move declaration of x above the println
+  public static void main(String[] args) {
+
+
+
+
+
+
+    System.out.println(x);
+    y =
+    int x = 1;
+  }
+}
